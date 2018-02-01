52643baab08162df15a3788d7fbb486e2263843a
yvan <57882634@qq.com>
2018-02-01 18:46:43 +0800

    继承

--- extends/inherit.js
+++ extends/inherit.js
@@ -1,5 +1,5 @@
 function inherit(p) {
-  if (p == null) throw  TypeError(); // p是一个对象但不能是null
+  if (p == null) throw TypeError(); // p是一个对象但不能是null
   if (Object.create) // 如果Object.create()存在
     return Object.create(p); // 直接使用它
   var t = typeof p; // 否则进一步检测类型
