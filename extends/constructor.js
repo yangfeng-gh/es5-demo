492d6e2fbb52f91283b6db2bde64f46c86b561e7
yvan <57882634@qq.com>
2018-02-01 18:51:22 +0800

    constructor

---
+++ extends/constructor.js
@@ -0,0 +1,13 @@
+/**
+ 函数的prototype属性是一个对象，这个对象拥有唯一一个不可枚举属性constructor
+ constructor属性的值是一个函数对象
+ */
+var F = function () {
+}; // 函数对象
+var p = F.prototype; // 这是F相关联的原型对象
+var c = p.constructor; // 这是与原型相关联的函数
+console.log(c === F); //true
+
+var o = new F();
+console.log(o.constructor === F); //true
+console.log(o.constructor === F.prototype.constructor); // true
---
+++ extends/constructor属性检测对象类型.js
@@ -0,0 +1,27 @@
+function typeAndValue(x) {
+  if (x == null) return ""; //Null和Undefined没有构造函数
+  switch (x.constructor) {
+    case Number:
+      return "Number" + x;
+      break;
+    case String:
+      return "String" + x;
+      break;
+    case Boolean:
+      return "Boolean" + x;
+      break;
+    case Date:
+      return "Date" + x;
+      break;
+    case RegExp:
+      return "Regexp" + x;
+      break;
+    case Complex:
+      return "Complex" + x;
+      break;
+  }
+}
+
+var today = new Date();
+console.log(today);
+console.log(today.constructor);
