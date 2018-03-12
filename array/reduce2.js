e811cc35c95049b571a38230775754bf38b9bbe7
yvan <57882634@qq.com>
2018-03-12 11:03:29 +0800

    继承

--- array/reduce2.js
+++ array/reduce2.js
@@ -11,15 +11,20 @@ function getWordCnt() {
   return obj;
 }
 
-console.log(getWordCnt());
+// console.log(getWordCnt());
 
 var arr = ["apple", "orange", "apple", "orange", "pear", "orange"];
 
-function getWordCnt() {
+function getWordCnt2() {
   return arr.reduce(function (previous, current) {
+    console.log(previous);
     previous[current] = (previous[current] + 1) || 1;
-    return prev;
+    return previous;
   }, {});
 }
+// 提供了初始值{}，则previous={}，current=arr[0]（即apple）
+// 未提供初始值时，previous='apple', current='orange'
 
-console.log(getWordCnt());
+// 总结：循环结束的条件是current=数组最后的元素，未提供初始值循环次数=arr.length-1，提供初始值会多一次循环
+
+getWordCnt2();
