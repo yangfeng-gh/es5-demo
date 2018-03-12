5cf0a777add95fe25775e3e52a9d454efa17ce38
yvan <57882634@qq.com>
2018-03-12 15:17:16 +0800

    继承

--- Funtion/闭包实现计时器.js
+++ Funtion/闭包实现计时器.js
@@ -1,9 +1,13 @@
-function count(){
-var n = 0;
-return {
-count: function(){return n++},
-reset: function(){n = 0}
-};
+function count() {
+  var n = 0;
+  return {
+    count: function () {
+      return n++
+    },
+    reset: function () {
+      n = 0
+    }
+  };
 }
 
 var a = count(), b = count();
