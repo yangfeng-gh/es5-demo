424a44510d9cbbb402339e4d7b70621016c74bb5
yvan <57882634@qq.com>
2018-03-13 10:48:38 +0800

    继承

--- extends/利用constructor属性检测对象类型.js
+++ extends/利用constructor属性检测对象类型.js
@@ -5,22 +5,22 @@ function typeAndValue(x) {
   if (x == null) return ""; //Null和Undefined没有构造函数
   switch (x.constructor) {
     case Number:
-      return "Number" + x;
+      return "[Number] " + x;
       break;
     case String:
-      return "String" + x;
+      return "[String] " + x;
       break;
     case Boolean:
-      return "Boolean" + x;
+      return "[Boolean] " + x;
       break;
     case Date:
-      return "Date" + x;
+      return "[Date] " + x;
       break;
     case RegExp:
-      return "Regexp" + x;
+      return "[Regexp] " + x;
       break;
     case Complex:
-      return "Complex" + x;
+      return "[Complex] " + x;
       break;
   }
 }
