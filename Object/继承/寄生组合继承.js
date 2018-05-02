5c7ee6f3425000bd28b160d5cf8e46bec5406dc4
yvan <57882634@qq.com>
2018-05-02 12:40:14 +0800

    继承

--- Object/继承/寄生组合继承.js
+++ Object/继承/寄生组合继承.js
@@ -7,17 +7,17 @@ function createObj(o) {
 function inheritPrototype(subType, superType) {
   var prototype = createObj(superType.prototype);
   prototype.constructor = subType;
+  prototype.sayName = function () {
+    console.log(this.name);
+  };
   subType.prototype = prototype;
 }
 
 function SuperType(name) {
   this.name = name;
-  this.colors = ['red', 'green', 'blue'];
+  this.friends = ['Linus Torvalds', 'Bjarne Stroustrup'];
 }
 
-SuperType.prototype.sayName = function () {
-  console.log(this.name);
-};
 
 function SubType(name, age) {
   SuperType.call(this, name);
@@ -29,3 +29,7 @@ inheritPrototype(SubType, SuperType);
 SubType.prototype.sayAge = function () {
   console.log(this.age);
 };
+
+var subType = new SubType('Dennis, Ritchie', 30);
+subType.sayName();
+subType.sayAge();
